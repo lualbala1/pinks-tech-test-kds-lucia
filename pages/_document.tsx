a11ab4ef06db9c13ba8224381f6dd896aef48720
lualbala1 <lucia.albala1@ingka.ikea.com>
2025-04-16 09:21:16 +0200

    Add favicon and update document head to include favicon link

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,14 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head/>
+      <Head>
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+      </Head>
       <body>
         <title>Pink's KDS: Krazy Display Service</title>
         <Main />
